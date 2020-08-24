aea91261ea2d186846339a875fdf79df964ad274
Natalya Apasova <natalya.gulak.apasova@yandex.by>
2020-08-24 16:09:52 +0300

    feat: add CardsCreationForm component and remove button to the Card component with styles styles: fix styls refactor: urls for images

--- src/react-homework01/src/components/Card/CardsContainer.js
+++ src/react-homework01/src/components/Card/CardsContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Card from './Card';
 import ApiCall from '../../api/mockedApi.js';
 import styles from './CardsContainer.module.sass';
+import CardsCreationForm from './CardsCreationForm';
 
 class CardsContainer extends Component {
   constructor(props) {
@@ -25,16 +26,21 @@ class CardsContainer extends Component {
     const { cards } = this.state;
     
     return (
-      cards.length === 0
-      ? <div> No cards yet </div>
-      : <div >
-          <div className={styles.CardsContainer}>
-          {
-            this.state.cards.map((item) => {
-              return (<Card key={item.id} data={item} />)
-           })
-          }
-        </div>
+      <div>
+        {
+          cards.length === 0
+          ? <div> No cards yet </div>
+          : <div>
+              <div className={styles.CardsContainer}>
+              {
+                this.state.cards.map((item) => {
+                  return (<Card key={item.id} data={item} />)
+               })
+              }
+            </div>
+          </div>
+        }
+        <CardsCreationForm />
       </div>
     )
   }
---
+++ src/react-homework01/src/components/Card/CardsCreationForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './CardsCreationForm.module.sass';
+
+export default function CardsCreationForm(props) {
+  return (
+    <div className={styles.CardsCreationForm}>
+      <input type="text" placeholder="id"></input>
+      <input type="text" placeholder="image URL"></input>
+      <input type="text" placeholder="title"></input>
+      <input type="text" placeholder="gender"></input>
+      <input type="text" placeholder="price"></input>
+      <button type="submit">Submit</button>
+    </div>
+  );
+}
+
+
