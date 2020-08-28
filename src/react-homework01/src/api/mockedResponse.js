f06d7b9d2104c04a8d1c685f673d2b3ae2c96bd8
Natalya Apasova <natalya.gulak.apasova@yandex.by>
2020-08-29 01:58:21 +0300

    style: fix styles refactor: add images, fix urls for local images

--- src/react-homework01/src/api/mockedResponse.js
+++ src/react-homework01/src/api/mockedResponse.js
@@ -3,56 +3,56 @@ export const mockedResponse = [
     id: 1,
     price: 256,
     title: 'Title here',
-    imageUrl: `https://www20.hizlipara.co/3031-4-large_default/Erkekler-2-adet-slim-fit-tak%C4%B1m-elbise-ceket-ve-pantolon-tak%C4%B1m-elbise-erkek-resmi-blazer-kost%C3%BCm-homme-d%C3%BC%C4%9F%C3%BCn-elbisesi-kraliyet-mavi.jpg`,
+    imageUrl: `assets/suit.jpg`,
     gender: 'male'
   },
   {
     id: 2,
     price: 425,
     title: 'Title here',
-    imageUrl: `https://i.pinimg.com/originals/cc/4f/00/cc4f002100105d419e2ad34f86164540.jpg`,
+    imageUrl: `assets/dress.jpg`,
     gender: 'female'
   },
   {
     id: 3,
     price: 156,
     title: 'Title here',
-    imageUrl: `https://i.pinimg.com/originals/cc/4f/00/cc4f002100105d419e2ad34f86164540.jpg`,
+    imageUrl: `assets/dress.jpg`,
     gender: 'female'
   },
   {
     id: 4,
     price: 90,
     title: 'Title here',
-    imageUrl: `https://www20.hizlipara.co/3031-4-large_default/Erkekler-2-adet-slim-fit-tak%C4%B1m-elbise-ceket-ve-pantolon-tak%C4%B1m-elbise-erkek-resmi-blazer-kost%C3%BCm-homme-d%C3%BC%C4%9F%C3%BCn-elbisesi-kraliyet-mavi.jpg`,
+    imageUrl: `assets/tshirt_top_sleeve.png`,
     gender: 'male'
   },
   {
     id: 5,
     price: 205,
     title: 'Title here',
-    imageUrl: `https://www20.hizlipara.co/3031-4-large_default/Erkekler-2-adet-slim-fit-tak%C4%B1m-elbise-ceket-ve-pantolon-tak%C4%B1m-elbise-erkek-resmi-blazer-kost%C3%BCm-homme-d%C3%BC%C4%9F%C3%BCn-elbisesi-kraliyet-mavi.jpg`,
+    imageUrl: `assets/suit.jpg`,
     gender: 'male'
   },
   {
     id: 6,
     price: 287,
     title: 'Title here',
-    imageUrl: `https://i.pinimg.com/originals/cc/4f/00/cc4f002100105d419e2ad34f86164540.jpg`,
+    imageUrl: `assets/tshirt.png`,
     gender: 'female'
   },
   {
     id: 7,
     price: 205,
     title: 'Title here',
-    imageUrl: `https://www20.hizlipara.co/3031-4-large_default/Erkekler-2-adet-slim-fit-tak%C4%B1m-elbise-ceket-ve-pantolon-tak%C4%B1m-elbise-erkek-resmi-blazer-kost%C3%BCm-homme-d%C3%BC%C4%9F%C3%BCn-elbisesi-kraliyet-mavi.jpg`,
+    imageUrl: `assets/suit.jpg`,
     gender: 'male'
   },
   {
     id: 8,
     price: 287,
     title: 'Title here',
-    imageUrl: `https://i.pinimg.com/originals/cc/4f/00/cc4f002100105d419e2ad34f86164540.jpg`,
+    imageUrl: `assets/dress.jpg`,
     gender: 'female'
   }
 ]
